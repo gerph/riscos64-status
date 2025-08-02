d629cdff66398af28221cbdc5478baa46a6e59a8
Charles Ferguson <gerph@gerph.org>
2025-08-02 01:46:03 +0100

    Update the phases to be more complete; generate gantt charts.
    The phases are now a little more completely specified, and I've started
    writing some notes on why they're that way. Only two sections are done
    yet, but it's getting there. The Gantt chart generation makes some quite
    useful charts that are a good starting point for people to discuss the
    work needed and how it might be organised.

---
+++ utils/plantogantt.pl
@@ -0,0 +1,198 @@
+#!/bin/perl
+##
+# Make gantt charts for the different sections.
+#
+
+use warnings;
+use strict;
+
+use JSON;
+
+my $format = shift;
+if (!defined $format)
+{
+    $format = 'json';
+}
+
+open(my $fh, '<', 'planning/Phases.md') || die "Cannot read Phases.md: $!\n";
+
+my $section;
+my $phase;
+my $intable = 0;
+my %headings;
+
+my $plan = {
+    'phases' => {},
+        # Phase# => {
+        #   'name' => name
+        #   'states' => { component => state }
+    'components' => {},
+        # Component => { phase# => state },
+    'stacks' => {},
+        # Stack => {
+        #   Component => { component field above }
+        # }
+};
+
+my @states = (
+    'No work',
+    'Investigate',
+    'Stub',
+    'Prototype',
+    'Built',
+    'Internals',
+    'Functional',
+    'Complete',
+    'Tested',
+    'Automated',
+);
+
+my $sections = [];
+
+while (<$fh>)
+{
+    chomp;
+    my $line = $_;
+    $line =~ s/^ +//;
+    $line =~ s/ +$//;
+    if ($line =~ /^## Phase (\d+): *(.*)$/)
+    {
+        $phase = $1;
+        my $name = $2;
+        $plan->{'phases'}->{$phase} = {
+                'name' => $name,
+                'states' => {}, # Component => state
+            };
+    }
+    if ($intable == 0)
+    {
+        # Check for the start of the table
+        if ($line =~ s/^\|//)
+        {
+            $intable = 1;
+            my @labels = map { my $l = $_;
+                               $l =~ s/^ +//;
+                               $l =~ s/ +$//;
+                               $l;
+                         } split /\|/, $line;
+            my $index = 0;
+            %headings = ();
+            for my $label (@labels)
+            {
+                $headings{$label} = $index;
+                $index++;
+            }
+        }
+    }
+    else
+    {
+        # We're in a table
+        if ($line eq '')
+        {
+            $intable = 0;
+        }
+        elsif ($line =~ /^\|-/)
+        {
+            # The heading divider
+        }
+        elsif (!defined $headings{'Stack'})
+        {
+            # Ignore any table line for tables that don't have a stack
+        }
+        elsif ($line =~ s/^\|//)
+        {
+            # Table row
+            my @labels = map { my $l = $_;
+                               $l =~ s/\[\^[^\]]+\]//g; # Remove footnotes
+                               $l =~ s/^ +//;
+                               $l =~ s/ +$//;
+                               $l;
+                         } split /\|/, $line;
+
+            my $name = $labels[$headings{'Name'}];
+            my $stack = $labels[$headings{'Stack'}];
+            my $state = $labels[$headings{'Functionality'}];
+
+            $stack =~ s!/!!; # Fix I/O!
+
+            # Phases
+            $plan->{'phases'}->{$phase}->{'states'}->{$name} = $state;
+
+            # Components
+            if (!defined $plan->{'components'}->{$name})
+            {
+                $plan->{'components'}->{$name} = {};
+            }
+            $plan->{'components'}->{$name}->{$phase} = $state;
+
+            # Stacks
+            if (!defined $plan->{'stacks'}->{$stack})
+            {
+                $plan->{'stacks'}->{$stack} = {};
+            }
+            if (!defined $plan->{'stacks'}->{$stack}->{$name})
+            {
+                $plan->{'stacks'}->{$stack}->{$name} = $plan->{'components'}->{$name};
+            }
+        }
+        else
+        {
+            die "Unrecognised table line: $line\n";
+        }
+    }
+}
+
+if ($format eq 'json')
+{
+    print encode_json($plan);
+}
+elsif ($format eq 'mmd')
+{
+    # Mermaid to show what the plan is
+    for my $stack (sort { $a cmp $b } keys %{ $plan->{'stacks'} })
+    {
+        my $filename = "planning/Stack-$stack.mmd";
+        open(my $ofh, '>', $filename) || die "Cannot write stack mermaid file '$filename': $!";
+        print $ofh <<EOM;
+---
+    # Frontmatter config, YAML comments
+    displayMode: compact
+    config:
+        theme: forest
+        gantt:
+            useWidth: 800
+            rightPadding: 0
+            leftPadding: 150
+            topAxis: true  #false
+            numberSectionStyles: 2
+---
+gantt
+    title Stack: $stack
+    dateFormat YYYY
+    axisFormat %y
+    tickInterval 12month
+
+EOM
+        print $ofh "\n";
+
+        for my $name (sort { $a cmp $b } keys %{ $plan->{'stacks'}->{$stack} })
+        {
+            my $comp = $plan->{'stacks'}->{$stack}->{$name};
+            print $ofh "    section $name\n";
+            for my $phase (sort { $a <=> $b } keys %$comp)
+            {
+                my $state = $comp->{$phase};
+                my $line = sprintf "    %-12s : , 190%i, 1y", $state, $phase;
+                print $ofh "$line\n";
+            }
+            print $ofh "\n";
+        }
+
+        #for my $phase (sort { $a cmp $b } keys %{ $plan->{'phases'} })
+        #{
+        #    print $ofh "    Phase $phase : vert, 190" .($phase) . ", 1w\n";
+        #}
+
+        close($ofh);
+    }
+}
